d88f48ee6d4ac7a68e8fab34c5f77b6505e558a5
willdodsonn <sodlax38@gmail.com>
2022-02-14 23:52:44 +0000

    messing around

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import NavBar from "./newbuttons";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
+			<NavBar></NavBar>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
